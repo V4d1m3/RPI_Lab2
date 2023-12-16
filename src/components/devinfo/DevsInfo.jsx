b87f8803bf16fdb15973f34094f8d8ac95fe9312
V4d1m3 <vapelikh12@gmail.com>
2023-12-17 02:03:03 +0300

    Added developers info / put node_modules in ignore

---
+++ src/components/devinfo/DevsInfo.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Dev from "../dev/Dev";
+import { useTranslation } from "react-i18next";
+
+import s from "./DevsInfo.module.css";
+
+const DevsInfo = (props) => {
+  const { t } = useTranslation();
+  return (
+    <div className={s.wrapper}>
+      <h1>{t("developers")}</h1>
+      <div className={s.list}>
+        {props.info.map((info, i) => (
+          <Dev key={i} info={info} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DevsInfo;
---
+++ src/components/devinfo/developersInfo.js
@@ -0,0 +1,28 @@
+const developersInfo = [
+  {
+    id: 1,
+    name: "Vadim",
+    github: "https://github.com/V4d1m3",
+    img_src: "https://avatars.githubusercontent.com/u/129615684?v=4",
+  },
+  {
+    id: 2,
+    name: "Nikita",
+    github: "https://github.com/Dedoff1",
+    img_src: "https://avatars.githubusercontent.com/u/128306784?v=4",
+  },
+  {
+    id: 3,
+    name: "Alexander",
+    github: "https://github.com/San43zS",
+    img_src: "https://avatars.githubusercontent.com/u/126359434?v=4",
+  },
+  {
+    id: 4,
+    name: "Ekaterina",
+    github: "https://github.com/Foxocl",
+    img_src: "https://avatars.githubusercontent.com/u/120170658?v=4",
+  },
+];
+
+export default developersInfo;
