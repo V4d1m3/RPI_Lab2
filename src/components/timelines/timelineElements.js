cd23447e6130457cde05fccf38e61ff691b67807
Foxocl <amiray701@gmail.com>
2023-12-17 02:33:27 +0300

    Added timelines of some scammers

---
+++ src/components/timelines/timelineElements.js
@@ -0,0 +1,9 @@
+import abignaleTimeline from "./abignaleTimeline";
+import mavrodyTimeline from "./mavrodyTimeline";
+
+let timelineElements = [
+    abignaleTimeline,
+    mavrodyTimeline
+  ];
+  
+export default timelineElements;
