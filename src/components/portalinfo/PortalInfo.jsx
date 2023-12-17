3ab6a2b679e06b3ad64cede7cc542e0077fcef31
Foxocl <amiray701@gmail.com>
2023-12-17 18:43:06 +0300

    Added Portalinfo on main page

---
+++ src/components/portalinfo/PortalInfo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import s from "./PortalInfo.module.css";
+import { useTranslation } from "react-i18next";
+
+const PortalInfo = () => {
+  const { t } = useTranslation();
+  let maintitle_en = "Famous Scammers";
+  const name = maintitle_en.toLowerCase().split(" ").join("_");
+  let portaltext_en = "Portal Text";
+  const text = portaltext_en.toLowerCase().split(" ").join("_");
+  return (
+    <div className={s.wrapper}>
+      <div className={s.name}>{t(name)}</div>
+      <div className={s.disc}>{t(text)}</div>
+    </div>
+  );
+};
+
+export default PortalInfo;
